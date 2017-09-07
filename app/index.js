82df3b0d57aaf834b1c19cfd16185ec07dddfe92
Samuel Li <samli@samuelli.net>
2017-09-08 01:38:36 +1000

    reading data file.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
-exports.myHandler = function(event, context, callback) {
-   console.log("value1 = " + event.key1);
-   console.log("value2 = " + event.key2);
-   callback(null, "some success message");
-   // or
-   // callback("some error type");
+'use strict';
+
+var fs = require('fs');
+
+exports.myHandler = function (event, context, callback) {
+    var contents = fs.readFileSync('./data/animals.json', 'utf8');
+
+    callback(null, JSON.parse(contents));
+    // or
+    // callback("some error type");
 };
