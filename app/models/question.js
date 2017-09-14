89b3c265c6220a0eb3be60af2a17a91f6ab0a195
Samuel Li <samli@samuelli.net>
2017-09-15 00:31:16 +1000

    refactored question class.

--- app/models/question.js
+++ app/models/question.js
@@ -7,14 +7,17 @@ function Question(field, possibleValues, chosenValue) {
 }
 
 Question.prototype.toText = function () {
-    if (this.field === 'types') {
-        return `Is it a ${this.chosenValue}?`;
-    } else if (this.field === "behaviours") {
-        return `Does it ${this.chosenValue}?`;
-    } else if (this.field === "physical") {
-        return `Does it have ${this.chosenValue}?`;
-    } else if (this.field === "diet") {
-        return `Does it eat ${this.chosenValue}?`;
+    switch (this.field) {
+        case "types":
+            return `Is it a ${this.chosenValue}?`;
+        case "behaviours":
+            return `Does it ${this.chosenValue}?`;
+        case "physical":
+            return `Does it have ${this.chosenValue}?`;
+        case "diet":
+            return `Does it eat ${this.chosenValue}?`;
+        default:
+            return "I don't know what to ask!";
     }
 };
 
