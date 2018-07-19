7e7e3d9ac5cc4605154bf2eaa115e7177238570a
Samuel Li <samli@samuelli.net>
2018-07-19 22:34:36 +1000

    more restrictive IAM role for the Lambda function.

--- scripts/cf_deploy_stack.sh
+++ scripts/cf_deploy_stack.sh
@@ -3,10 +3,14 @@ set -e
 
 source `dirname "$0"`/utils.sh
 
-S3_BUCKET=$1
-NOTIFICATION_TOPIC_ARN=$2
-BUILD_NUMBER=$3    # build number (e.g. "9c1a339" or "9c1a339-snapshot")
+BUILD_NUMBER=$1    # build number (e.g. "9c1a339" or "9c1a339-snapshot")
+S3_BUCKET=$2
+DATA_S3_BUCKET=$3
 STACK_NAME=$4
+AWS_ACCOUNT_ID=$5
+AWS_REGION=$6
+NOTIFICATION_TOPIC_ARN="arn:aws:sns:${AWS_REGION}:${AWS_ACCOUNT_ID}:prof_animal_lost_game"
+SESSION_DB=arn:aws:dynamodb:${AWS_REGION}:${AWS_ACCOUNT_ID}:table/AnimalGenieUserSession
 S3_KEY=$(s3_key_from_build ${BUILD_NUMBER})
 STACK_NAME="ProfessorAnimal-${STACK_NAME}"
 
@@ -22,5 +26,8 @@ aws  cloudformation deploy \
      --parameter-overrides \
         keyToBuildArtefact=${S3_KEY} \
         bucketName=${S3_BUCKET} \
-        notificationTopicArn=${NOTIFICATION_TOPIC_ARN} && \
-    aws cloudformation describe-stacks --stack-name ${STACK_NAME}
+        notificationTopicArn=${NOTIFICATION_TOPIC_ARN} \
+        dataBucket=${DATA_S3_BUCKET} \
+        sessionDB=${SESSION_DB} && \
+    aws cloudformation describe-stacks --stack-name ${STACK_NAME} \
+        --query 'Stacks[0].Outputs[?OutputKey==`ProfessorAnimalApi`]'
